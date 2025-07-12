e65ee0c4a961688ea2fb0df49095bf4851cdd620
Daniel <daniel.pool.2024@gmail.com>
2025-07-12 19:15:28 +0900

    Implemented Catch2 and CMakeLists.txt

---
+++ test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by danie on 7/12/2025.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include "document.h"
+using namespace std;
+TEST_CASE("EXAMPLE", "[Example]") {
+    REQUIRE(1+1 ==2);
+}
