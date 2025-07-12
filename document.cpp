1713c3c3df0ace53cb5178f902f6c34a347c7cbc
Daniel <daniel.pool.2024@gmail.com>
2025-07-12 22:54:50 +0900

    Updated CMakeLists.txt and Implemented test_suffix_array.cpp

--- document.cpp
+++ document.cpp
@@ -1,5 +1,7 @@
 #include "document.h"
 
+#include <iostream>
+
 // Constructor for the Document class.
 // Initializes a Document object with a unique id and its content.
 Document::Document(int id, const std::string& content)
